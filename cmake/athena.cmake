8da3edadc58d722177c550ec88a27ddaad3d3c38
mac/cli <chengcli@umich.edu>
2024-07-16 10:45:04 -0400

    add athena patch

--- cmake/athena.cmake
+++ cmake/athena.cmake
@@ -14,7 +14,8 @@ set(patch_command
     ${CMAKE_CURRENT_SOURCE_DIR}/patches/28.bvals_cc.patch
     ${CMAKE_CURRENT_SOURCE_DIR}/patches/29.mesh.patch
     ${CMAKE_CURRENT_SOURCE_DIR}/patches/30.bvals_base.patch
-    ${CMAKE_CURRENT_SOURCE_DIR}/patches/31.task_list.patch)
+    ${CMAKE_CURRENT_SOURCE_DIR}/patches/31.task_list.patch
+    ${CMAKE_CURRENT_SOURCE_DIR}/patches/xxx.bvals_hack.patch)
 
 FetchContent_Declare(
   athenapp
