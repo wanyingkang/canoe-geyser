fb5f9cc7b5452b762bf993556256649b5df1ccd2
wanyingkang <kangwanying1992@gmail.com>
2024-07-18 11:37:43 -0400

    minor changes 2

---
+++ lava/examples/2024-Emeline-Mariana-lava/CMakeFiles/lava.release.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/wanying/canoe/main.cpp" "examples/2024-Emeline-Mariana-lava/CMakeFiles/lava.release.dir/__/__/main.cpp.o" "gcc" "examples/2024-Emeline-Mariana-lava/CMakeFiles/lava.release.dir/__/__/main.cpp.o.d"
+  "/home/wanying/canoe/examples/2024-Emeline-Mariana-lava/lava.cpp" "examples/2024-Emeline-Mariana-lava/CMakeFiles/lava.release.dir/lava.cpp.o" "gcc" "examples/2024-Emeline-Mariana-lava/CMakeFiles/lava.release.dir/lava.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
